0694b6c45f5687be18dc0906cb2c2beeeb3280d1
astral0525 <148305868+astral0525@users.noreply.github.com>
2023-12-08 09:39:27 +0900

    [level unrated] Title: 문자열 잘라서 정렬하기, Time: 19.03 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -4,10 +4,10 @@ class Solution {
     public String[] solution(String myString) {
 
         String[] answer = myString.split("x");
-        String[] answer1 = Arrays.stream(answer)
-                .filter(s -> !s.isEmpty())
-                .toArray(String[]::new);
-        Arrays.sort(answer1);
-        return answer1;
+        answer = Arrays.stream(answer)
+                       .filter(s -> !s.isEmpty())
+                       .toArray(String[]::new);
+        Arrays.sort(answer);
+        return answer;
     }
 }
